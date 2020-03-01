7042a834e1ae5c7edac2c91ad7a505b24182ed5e
Amy Fabella <amyfabella@hotmail.com>
2020-02-29 22:47:11 -0800

    creating files

---
+++ index.js
@@ -0,0 +1,32 @@
+const api = require("./api");
+const generateMarkdown = require("./generateMarkdown");
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
+
+// * The generated README includes a bio image from the user's GitHub profile.
+
+// * The generated README includes the user's email.
+
+// * The generated README includes the following sections: 
+//   * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//   * Usage
+//   * License
+//   * Contributing
+//   * Tests
+//   * Questions
+
+// * The generated README includes 1 badge that's specific to the repository.
